6e08e9cd96e0eb56e594861c574ad74055557cc5
Melanie Fryman <melanie@frymanet.com>
2014-09-17 14:53:54 -0500

    weather directive

---
+++ client/components/directives/mf-weather/mf-weather.js
@@ -0,0 +1,31 @@
+/* jshint camelcase:false */
+
+(function(){
+  'use strict';
+
+  angular.module('mfWeatherModule', [])
+  .factory('WeatherApi', ['$http', function($http){
+    function conditions(zip){
+      return 'http://api.wunderground.com/api/f62d84d2f24e71aa/conditions/q/' + zip + '.json?callback=JSON_CALLBACK';
+    }
+    return {conditions:conditions};
+  }])
+
+  .directive('mfWeather', ['$interval', function($interval){
+    var o = {};
+
+    o.restrict = 'A';
+    o.templateUrl = '/components/directives/mf-weather/mf-weather.html';
+    o.scope = {zip:'@'};
+    o.link = function(scope, element, attrs){
+             };
+
+    o.controller = ['$scope', 'WeatherApi', function($scope, WeatherApi){
+        WeatherApi.conditions($scope.zip).then(function(response){
+          $scope.conditions = response.data.current_observation;
+        });
+      }
+    }];
+    return o;
+  }]);
+})();
--- client/views/home/home.js
+++ client/views/home/home.js
@@ -5,6 +5,7 @@
   .controller('HomeCtrl', ['$scope', '$interval', 'Home', function($scope, $interval, Home){
     $scope.people = [{name:'Fluffy', age:'5'}, {name:'Snowball', age:'7'}, {name:'Ginger', age:'10'}];
     $scope.symbols = ['AAPL', 'GOOG', 'MSFT', 'AMZN'];
+    $scope.zips = ['37219', '75711', '38004'];
   }]);
 })();
 
